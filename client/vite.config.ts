f63eeb95f1459f4174de4d85b3e40790eae076fd
FreadSt <thiswolfram@gmail.com>
2025-11-19 17:22:56 +0200

    fix config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.message.includes("'use client'")) return;
+        warn(warning);
+      },
+    },
+  },
 });
